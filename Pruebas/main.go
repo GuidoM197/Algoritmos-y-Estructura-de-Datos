d032d472e0eaa197572e8b081b8e968db9b419ac
GuidoM197 <Guidomazzucco197@gmail.com>
2024-03-20 14:25:16 -0300

    Todo ok

--- Pruebas/main.go
+++ Pruebas/main.go
@@ -1,62 +1,47 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+)
 
-func Comparar(vector1 []int, vector2 []int) int {
-  // Verifico que ninguno sea un areglo vacio para meter un early return en caso de ser necesario para no ejecutar codigo de mas.
-  if len(vector1) == 0 && len(vector2) != 0 {
+func Swap(x *int, y *int) {
+  *x, *y = *y, *x
+}
+
+func Maximo(vector []int) int {
+  if len(vector) == 0 {
     return -1
-  } else if len(vector1) != 0 && len(vector2) == 0 {
-    return 1
-  } else if len(vector1) == 0 && len(vector2) == 0 {
-    return 0
   }
 
-  // Determino si hay algun arreglo con menos longitud de antemano y lo utilizo para comparar los elementos en ese rango, tambien me guardo el numero del mas largo.
-  longitud_max := 0
-  vector_mas_largo := 0
+  maximo, posicionMax := vector[0], 0
 
-  if len(vector1) > len(vector2) {
-    longitud_max = len(vector2)
-    vector_mas_largo = 1
-  } else if len(vector1) < len(vector2) {
-    longitud_max = len(vector1)
-    vector_mas_largo = 2
-  } else {
-    // Ya que ambos vectores tendrian el mismo tamaño elijo cualquiera de los dos.
-    longitud_max = len(vector1) 
-  }
-
-  // En este punto es indiferente que arreglo usar como referencia de len ya que son del mismo tamaño.
-  for i := 0; i < longitud_max; i++ { 
-
-    elemento_v1, elemento_v2 := vector1[i], vector2[i]
-
-    if elemento_v1 > elemento_v2 {
-        return 1
-    } else if elemento_v1 < elemento_v2 {
-        return -1
+  for i := 0; i < len(vector); i++ {
+    
+    if vector[i] > maximo {
+      posicionMax = i
+      maximo = vector[i]
     }
-	
   }
 
-  // Si no tienen elementos mas grandes determino el mayor arreglo segun su tamaño.
-  if vector_mas_largo == 1 {
-    return 1
-  } else if vector_mas_largo == 2 {
-    return -1
+  return posicionMax
+
+}
+
+func Seleccion(vector []int) {
+
+  for i := (len(vector) - 1); i > 0; i-- {
+    posicionMax := Maximo(vector[:i+1])
+    Swap(&vector[i], &vector[posicionMax])
+
   }
 
-  // Serian iguales. 
-  return 0
-  
 }
 
 func main() {
 
-  a := []int{10}
-  b := []int{10}
-  // Comparar(a, b)
-  fmt.Println(Comparar(a, b))
-  fmt.Println(Comparar(b, a))
+  arr := []int{1,2,3,4,5}
+  Seleccion(arr)
+  fmt.Println(arr)
+
+
 }
---
+++ tp0/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"tp0/ejercicios"
+)
+
+const rutaArch1 = "archivo1.in"
+const rutaArch2 = "archivo2.in"
+
+func recolectorDeDatos(ruta string) []int {
+
+	archivo, _ := os.Open(ruta)
+	defer archivo.Close()
+
+	lineas := bufio.NewScanner(archivo)
+
+	slice_aux := []string{}
+
+	for lineas.Scan() {
+		slice_aux = append(slice_aux, lineas.Text())
+	}
+
+	slice_final := []int{}
+
+	for _, valor := range slice_aux {
+		numero, _ := strconv.Atoi(valor)
+		slice_final = append(slice_final, numero)
+	}
+
+	return slice_final
+}
+
+func main() {
+
+	arreglo1 := recolectorDeDatos(rutaArch1)
+	arreglo2 := recolectorDeDatos(rutaArch2)
+
+	mayorArreglo := ejercicios.Comparar(arreglo1, arreglo2)
+
+	if mayorArreglo == 1 {
+		ejercicios.Seleccion(arreglo1)
+		fmt.Println(arreglo1)
+	} else if mayorArreglo == -1 {
+		ejercicios.Seleccion(arreglo2)
+		fmt.Println(arreglo2)
+	} else if mayorArreglo == 0 {
+		fmt.Println("Son del mismo tamaño.")
+	}
+
+}
