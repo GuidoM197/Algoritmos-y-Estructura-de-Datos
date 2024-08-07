ac5bf367e8023a9ece6c1dedc3b5122aa22ca982
GuidoM197 <gmazzucco@fi.uba.ar>
2024-08-06 22:20:50 -0300

    Todo en funcionamiento

--- tp1/operations/operator.go
+++ tp1/operations/operator.go
@@ -2,11 +2,4 @@ package operations
 
 type Operator interface {
 	Operation() (int64, error)
-	//OperateSum() (int64,error)
-	//OperateSubtraction() (int64,error)
-	//OperateMultiplication() (int64,error)
-	//OperateDivision() (int64,error)
-	//OperateLogarithm() (int64,error)
-	//OperateSqrt() (int64,error)
-	//OperateTernary() (int64,error)
 }
